a1d1a11864eefc465f2c508a0854dc536f7baf2d
ramirocarusi13 <rami_carusi@hotmail.com.ar>
2023-03-02 21:01:08 -0300

    update

--- src/app.js
+++ src/app.js
@@ -2,13 +2,89 @@ import express from 'express'
 import productsRouter from './routes/products.router.js'
 import cartsRouter from './routes/carts.router.js'
 import './persistencia/dbConfig.js'
+import cookieParser from 'cookie-parser'
+import session from 'express-session'
+import { __dirname } from './utils.js'
+import handlebars from 'express-handlebars'
+import viewsRouter from './routes/views.router.js'
+import usersRouter from './routes/users.router.js'
+import './persistencia/dbConfig.js'
 const app = express()
+
+// file session
+import FileStore from 'session-file-store'
+const fileStore = FileStore(session)
+
+// mongo session
+import mongoStore from 'connect-mongo'
+
+
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use('/products', productsRouter)
-app.use('/carts', cartsRouter)
-const PORT = 8080
+app.use(cookieParser())
+
+// handlebars
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname + '/views')
+app.set('view engine', 'handlebars')
+
+
+// session File
+// app.use(
+//   session({
+//     store: new fileStore({
+//       path: __dirname + '/sessions',
+//     }),
+//     resave: false,
+//     saveUninitialized: false,
+//     secret: 'sessionKey',
+//     cookie:{maxAge:60000}
+//   })
+//   )
+
+// session Mongo
+app.use(
+    session({
+        store: new mongoStore({
+            mongoUrl: 'mongodb+srv://ramiro:coderhouse@clusterrc.wwlwaqy.mongodb.net/Sessions4561?retryWrites=true&w=majority'
+        }),
+        resave: false,
+        saveUninitialized: false,
+        secret: 'sessionKey',
+        cookie: { maxAge: 60000 }
+    })
+)
+
+// routes
+app.use('/views', viewsRouter)
+app.use('/users', usersRouter)
+app.get('/', (req, res) => {
+    res.redirect('/views/login')
+})
+
+// RUTA SESSION
+// app.post('/session',(req,res)=>{
+//     //console.log(req.body) // username - password
+//     const {username,password} = req.body
+//     req.session.username = username
+//     req.session.password = password
+//     res.json({message:'Sesion iniciada con exito'})
+// })
+
+// app.get('/logout',(req,res)=>{
+//     req.session.destroy((error)=>{
+//         if(error)console.log(error);
+//         else console.log('Sesion finalizada');
+//     })
+// })
+
+
+
+
+
+
+const PORT = 3000
 
 app.listen(PORT, () => {
     console.log(`Escuchando al puerto ${PORT}`)
---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,23 @@
+export function auth(req, res, next) {
+    if (req.session.logged) {
+        next()
+    } else {
+        res.redirect('/')
+    }
+}
+
+export function isLogged(req, res, next) {
+    if (req.session.logged) {
+        res.redirect('/views/perfil')
+    } else {
+        next()
+    }
+}
+
+export function isAdmin(req, res, next) {
+    if (req.session.isAdmin) {
+        res.redirect('/views/admin')
+    } else {
+        next()
+    }
+}
